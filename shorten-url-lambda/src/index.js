fbd3f951c69caf3b8cbc9c60c9b4e0e93e4591da
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-03-29 10:52:46 +0100

    URL shortening logic

--- shorten-url-lambda/src/index.js
+++ shorten-url-lambda/src/index.js
@@ -1,3 +1,49 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const client = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+
 export const handler = async (event) => {
     console.debug('Received event:', JSON.stringify(event, null, 2));
+
+    try {
+        const command = new PutCommand({
+            TableName: 'ShortenedUrls',
+            Item: createShortenUrlObject(event)
+        });
+        console.log('Command: ', command);
+
+        const result = await client.send(command);
+        return buildResponse(200, result);
+    } catch (err) {
+        console.error('Error: ', err);
+        return buildResponse(400, err.message);
+    }
 };
+
+const createShortenUrlObject = (event) => ({
+    'id': crypto.randomUUID(),
+    'code': generateCode(),
+    'longUrl': event.longUrl,
+    'createdAt': new Date().toUTCString(),
+});
+
+const generateCode = (length = 8) => {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+    let code = '';
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      code += chars[randomIndex];
+    }
+  
+    return code;
+};
+  
+const buildResponse = (statusCode, body) => ({
+    statusCode,
+    body: JSON.stringify(body),
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
