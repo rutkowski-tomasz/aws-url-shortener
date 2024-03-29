fca43aed63f1f2a4dc9796905482583a2fcd569d
Tomasz Rutkowski <11985426+rutkowski-tomasz@users.noreply.github.com>
2024-03-29 16:06:27 +0100

    9: Implement get-url-lambda (#14)
    * Implement get-url-lambda
    
    * Adjust workflow for ARM testing
    
    * Add region

---
+++ get-url-lambda/src/lambda_function.py
@@ -0,0 +1,30 @@
+import boto3
+from botocore.exceptions import ClientError
+
+dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
+table = dynamodb.Table('ShortenedUrls')
+
+def lambda_handler(event, context):
+    try:
+        code = event.get('code', '')
+        
+        response = table.get_item(Key={'code': code})
+        
+        if 'Item' in response:
+            return {
+                'statusCode': 302,
+                'headers': {
+                    'Location': response['Item']['longUrl']
+                }
+            }
+        else:
+            return {
+                'statusCode': 404,
+                'body': 'URL not found'
+            }
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return {
+            'statusCode': 500,
+            'body': 'Internal server error'
+        }
