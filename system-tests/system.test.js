9571407026ea101a454c545626159ad899209038
Tomasz Rutkowski <11985426+rutkowski-tomasz@users.noreply.github.com>
2024-05-01 14:47:46 +0200

    16: system-tests integrate with Cognito (#32)

--- system-tests/system.test.js
+++ system-tests/system.test.js
@@ -1,13 +1,49 @@
 const axios = require('axios');
 
 const urlShortenerBaseUrl = 'https://d0ghygtws1.execute-api.eu-central-1.amazonaws.com/dev';
+const cognitoUrl = 'https://cognito-idp.eu-central-1.amazonaws.com';
+
+const clientId = '4np6oaiu11oom6khgturukdfus';
+const username = 'system-tests@example.com';
+const password = 'Password123!';
 
 describe('Lambda function integration', () => {
   let code;
+  let idToken;
   const longUrl = 'https://example.com';
 
+  beforeAll(async () => {
+    async function fetchCognitoToken() {
+      try {
+        const response = await axios.post(cognitoUrl, {
+          "AuthFlow": "USER_PASSWORD_AUTH",
+          "ClientId": clientId,
+          "AuthParameters": {
+            "USERNAME": username,
+            "PASSWORD": password
+          }
+        }, {
+          headers: {
+            'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth',
+            'Content-Type': 'application/x-amz-json-1.1'
+          }
+        });
+        return response.data.AuthenticationResult.IdToken;
+      } catch (error) {
+        console.error('Error fetching Cognito token:', error);
+        throw error;
+      }
+    }
+
+    idToken = await fetchCognitoToken();
+  });
+
   test('shorten-url-lambda creates a short code for a URL', async () => {
-    const response = await axios.post(`${urlShortenerBaseUrl}/shorten-url`, { longUrl });
+    const response = await axios.post(`${urlShortenerBaseUrl}/shorten-url`, { longUrl }, {
+      headers: {
+        'Authorization': `Bearer ${idToken}`
+      }
+    });
     expect(response.status).toBe(200);
     expect(response.data.result).toHaveProperty('code');
     code = response.data.result.code;
