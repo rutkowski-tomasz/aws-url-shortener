a41e8788ce6c264d2848d413ddb3d947812ee716
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-09-09 12:23:04 +0200

    feat: system-test integrate with workflows

--- system-tests/system.test.js
+++ system-tests/system.test.js
@@ -1,26 +1,33 @@
 const axios = require('axios');
 
-const urlShortenerBaseUrl = 'https://d0ghygtws1.execute-api.eu-central-1.amazonaws.com/dev';
-const cognitoUrl = 'https://cognito-idp.eu-central-1.amazonaws.com';
+let config = {
+  urlShortenerBaseUrl: 'https://09fh8d9q9j.execute-api.eu-central-1.amazonaws.com/dev',
+  cognitoUrl: 'https://cognito-idp.eu-central-1.amazonaws.com',
+  clientId: '1kirlbqnjpertctpamfatsfolc',
+  username: 'system-tests@example.com',
+  password: 'SecurePassword123!'
+}
 
-const clientId = '4np6oaiu11oom6khgturukdfus';
-const username = 'system-tests@example.com';
-const password = 'Password123!';
+if (process.env.environment == 'prd')
+{
+  config.urlShortenerBaseUrl = 'https://11428gw63m.execute-api.eu-central-1.amazonaws.com/prd';
+  config.clientId = '1chdujtpbetje7g3jine9cgdmg';
+}
 
 describe('Lambda function integration', () => {
   let code;
   let idToken;
-  const longUrl = 'https://example.com';
+  const longUrl = 'https://github.com/rutkowski-tomasz/ExpenseSplitter.Api';
 
   beforeAll(async () => {
     async function fetchCognitoToken() {
       try {
-        const response = await axios.post(cognitoUrl, {
+        const response = await axios.post(config.cognitoUrl, {
           "AuthFlow": "USER_PASSWORD_AUTH",
-          "ClientId": clientId,
+          "ClientId": config.clientId,
           "AuthParameters": {
-            "USERNAME": username,
-            "PASSWORD": password
+            "USERNAME": config.username,
+            "PASSWORD": config.password
           }
         }, {
           headers: {
@@ -39,7 +46,7 @@ describe('Lambda function integration', () => {
   });
 
   test('shorten-url-lambda creates a short code for a URL', async () => {
-    const response = await axios.post(`${urlShortenerBaseUrl}/shorten-url`, { longUrl }, {
+    const response = await axios.post(`${config.urlShortenerBaseUrl}/shorten-url`, { longUrl }, {
       headers: {
         'Authorization': `Bearer ${idToken}`
       }
@@ -47,14 +54,23 @@ describe('Lambda function integration', () => {
     expect(response.status).toBe(200);
     expect(response.data.result).toHaveProperty('code');
     code = response.data.result.code;
-  });                                 
+
+    console.log('Shortened URL code: ', code);
+  });
 
   test('get-url-lambda redirects to the original URL using the short code', async () => {
-    const response = await axios.get(`${urlShortenerBaseUrl}/get-url?code=${code}`, {
+    const response = await axios.get(`${config.urlShortenerBaseUrl}/get-url?code=${code}`, {
       validateStatus: () => true,
       maxRedirects: 0
     });
     expect(response.status).toBe(302);
     expect(response.headers.location).toBe(longUrl);
+  });   
+
+  test('get-preview-url-lambda returns preview URLs', async () => {
+    const response = await axios.get(`${config.urlShortenerBaseUrl}/get-preview-url?code=${code}`, );
+    expect(response.status).toBe(200);
+    expect(response.data.result).toHaveProperty('desktopPreview');
+    expect(response.data.result).toHaveProperty('mobilePreview');
   });
 });
