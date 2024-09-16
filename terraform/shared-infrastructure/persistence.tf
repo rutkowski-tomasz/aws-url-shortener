7aea4f88bd1fa29f5baa60d282da739598ca5f50
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-09-16 14:59:47 +0200

    feat: create connections ddb table

--- terraform/shared-infrastructure/persistence.tf
+++ terraform/shared-infrastructure/persistence.tf
@@ -15,3 +15,14 @@ resource "aws_dynamodb_table" "url_shortener" {
   stream_enabled   = true
   stream_view_type = "NEW_IMAGE"
 }
+
+resource "aws_dynamodb_table" "websocket_connections" {
+  name         = "${local.prefix}websocket-connections"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "userId"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+}
