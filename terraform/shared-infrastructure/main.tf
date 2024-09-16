3a08350e8d1cb255f755502b961893eb1ffedba5
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-09-16 19:56:00 +0200

    feat: create WS API Gateway

--- terraform/shared-infrastructure/main.tf
+++ terraform/shared-infrastructure/main.tf
@@ -29,9 +29,17 @@ provider "aws" {
 data "aws_region" "current" { }
 
 locals {
-  prefix      = "us-${local.environment}-"
-  environment = terraform.workspace == "prd" ? terraform.workspace : "dev"
-  project     = "shared-infrastructure"
+  is_valid_workspace = contains(["dev", "prd"], terraform.workspace)
+  prefix             = "us-${local.environment}-"
+  environment        = terraform.workspace
+  project            = "shared-infrastructure"
+}
+
+resource "null_resource" "validate_workspace" {
+  count = local.is_valid_workspace ? 0 : 1
+  provisioner "local-exec" {
+    command = "echo Invalid workspace: '${terraform.workspace}'. Must be either 'dev' or 'prd'. && exit 1"
+  }
 }
 
 output "cognito_user_pool_client_id" {
@@ -44,4 +52,12 @@ output "cognito_url" {
 
 output "api_gateway_health_url" {
   value = "${aws_api_gateway_deployment.deployment.invoke_url}${local.environment}/health"
+}
+
+output "ws_api_gateway_connect_url" {
+  value = aws_apigatewayv2_stage.stage.invoke_url
+}
+
+output "ws_api_gateway_api_id" {
+  value = aws_apigatewayv2_api.websocket_api.id
 }
