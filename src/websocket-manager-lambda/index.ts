d90d92bf2038eb972f81c6f11ff0fb22bf81742b
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-09-17 14:36:10 +0200

    feat: change structure of connections ddb

--- src/websocket-manager-lambda/index.ts
+++ src/websocket-manager-lambda/index.ts
@@ -1,20 +1,17 @@
-import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { PutCommand, DynamoDBDocumentClient, GetCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { PutCommand, DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
 
 const dynamoDbClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
 const { ENVIRONMENT } = process.env;
 const TableName = `us-${ENVIRONMENT}-websocket-connections`;
 
-export const handler: APIGatewayProxyHandler = async (event): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (
+    event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
     console.log('Event: %j', event);
-
-    const routeKey = event.requestContext.routeKey;
-    console.log('routeKey: %j', routeKey);
-    const eventType = event.requestContext.eventType;
-    console.log('eventType: %j', eventType);
 
     if (!event.requestContext.authorizer) {
         throw new Error('Authorizer is not set');
@@ -24,28 +21,17 @@ export const handler: APIGatewayProxyHandler = async (event): Promise<APIGateway
         throw new Error('ConnectionId is not set');
     }
 
+    const eventType = event.requestContext.eventType;
+    const userId = event.requestContext.authorizer.sub;
+    const connectionId = event.requestContext.connectionId;
+
     try {
-        const userId = event.requestContext.authorizer.sub;
-        const connectionId = event.requestContext.connectionId;
 
-        console.debug('userId: %j', userId);
-        console.debug('connectionId: %j', connectionId);
-
-        const userConnections = await getUserConnections(userId);
-        console.debug('userConnections: %j', userConnections);
-
-        if (!userConnections) {
-            console.debug('Inserting new connection');
-            const command = buildInsertConnectionCommand(userId, connectionId);
-            console.debug('command: %j', command);
-            const result = await dynamoDbClient.send(command);
-            console.debug('result: %j', result);
-        } else {
-            console.debug('Appending new connection');
-            const command = buildAddConnectionCommand(userId, [...userConnections.Item.connections, connectionId]);
-            console.debug('command: %j', command);
-            const result = await dynamoDbClient.send(command);
-            console.debug('result: %j', result);
+        if (eventType === 'CONNECT') {
+            await connectUser(userId, connectionId);
+        }
+        else if (eventType === 'DISCONNECT') {
+            await disconnectUser(connectionId);
         }
 
     } catch (err) {
@@ -56,36 +42,28 @@ export const handler: APIGatewayProxyHandler = async (event): Promise<APIGateway
     return { statusCode: 200, body: JSON.stringify({ connectionId: event.requestContext.connectionId }) };
 };
 
-const getUserConnections = async (userId: string) => {
+const disconnectUser = async (connectionId: string) => {
 
-    const command = new GetCommand({
+    const command = new DeleteCommand({
         TableName,
-        Key: { userId }
-    })
+        Key: { connectionId }
+    });
 
     const result = await dynamoDbClient.send(command);
-    console.debug('result: %j', result);
-
-    return result.Item;
+    console.debug('disconnecting user result: %j', result);
 }
 
-const buildInsertConnectionCommand = (userId: string, connectionId: string) => new PutCommand({
-    TableName,
-    Item: {
-        userId,
-        connectionIds: [connectionId],
-        createdAt: new Date().getTime(),
-    }
-})
+const connectUser = async (userId: string, connectionId: string) => {
 
-const buildAddConnectionCommand = (userId: string, connectionIds: string[]) => new UpdateCommand({
-    TableName,
-    Key: { userId },
-    UpdateExpression: "set connectionIds = :connectionIds",
-    ExpressionAttributeValues: {
-        ":connectionIds": connectionIds,
-    },
-    ReturnValues: "ALL_NEW",
-});
+    const command = new PutCommand({
+        TableName,
+        Item: {
+            userId,
+            connectionId,
+            createdAt: new Date().getTime(),
+        }
+    });
 
-
+    const result = await dynamoDbClient.send(command);
+    console.debug('connecting user result: %j', result);
+};
--- src/websocket-manager-lambda/main.tf
+++ src/websocket-manager-lambda/main.tf
@@ -42,20 +42,17 @@ resource "null_resource" "validate_workspace" {
 ###
 
 module "lambda" {
-  source                                = "../../terraform/modules/lambda"
-  environment                           = local.environment
-  lambda_function_name                  = local.project
-  lambda_handler                        = "index.handler"
-  lambda_runtime                        = "nodejs20.x"
-  ws_api_gateway_route                  = "$connect"
-  ws_api_gateway_requires_authorization = true
+  source               = "../../terraform/modules/lambda"
+  environment          = local.environment
+  lambda_function_name = local.project
+  lambda_handler       = "index.handler"
+  lambda_runtime       = "nodejs20.x"
 
   custom_policy_statements = [
     {
-      Action   = [
+      Action = [
         "dynamodb:PutItem",
-        "dynamodb:DeleteItem",
-        "dynamodb:GetItem"
+        "dynamodb:DeleteItem"
       ],
       Resource = data.aws_dynamodb_table.websocket_connections.arn
     }
@@ -64,4 +61,69 @@ module "lambda" {
 
 data "aws_dynamodb_table" "websocket_connections" {
   name = "${local.prefix}websocket-connections"
+}
+
+
+###
+
+data "terraform_remote_state" "shared_infrastructure" {
+  backend   = "s3"
+  workspace = terraform.workspace
+  config = {
+    bucket = "us-cicd"
+    key    = "terraform/shared-infrastructure"
+    region = "eu-central-1"
+  }
+}
+
+data "external" "ws_authorizer_id" {
+  program = ["sh", "-c", "aws apigatewayv2 get-authorizers --api-id ${data.terraform_remote_state.shared_infrastructure.outputs.ws_api_gateway_api_id} --query 'Items[?Name==`${local.prefix}websocket-cognito-authorizer`]|[0].{Id:AuthorizerId}'"]
+}
+
+data "aws_apigatewayv2_api" "websocket_api" {
+  api_id = data.terraform_remote_state.shared_infrastructure.outputs.ws_api_gateway_api_id
+}
+
+resource "aws_apigatewayv2_route" "connect" {
+  api_id             = data.terraform_remote_state.shared_infrastructure.outputs.ws_api_gateway_api_id
+  route_key          = "$connect"
+  authorization_type = "CUSTOM"
+  authorizer_id      = data.external.ws_authorizer_id.result.Id
+  target             = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "disconnect" {
+  api_id    = data.terraform_remote_state.shared_infrastructure.outputs.ws_api_gateway_api_id
+  route_key = "$disconnect"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
+
+resource "aws_apigatewayv2_integration" "integration" {
+  api_id           = data.terraform_remote_state.shared_infrastructure.outputs.ws_api_gateway_api_id
+  integration_type = "AWS_PROXY"
+  integration_uri  = module.lambda.lambda_function_arn
+}
+
+resource "aws_apigatewayv2_deployment" "deployment" {
+  api_id = data.terraform_remote_state.shared_infrastructure.outputs.ws_api_gateway_api_id
+  depends_on = [
+    aws_apigatewayv2_route.connect,
+    aws_apigatewayv2_route.disconnect,
+  ]
+
+  triggers = {
+    redeployment = timestamp()
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lambda_permission" "allow_api_gateway_invoke_lambda_authorizer" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${data.aws_apigatewayv2_api.websocket_api.execution_arn}/${local.environment}/*"
 }
