15594e7834526a89135cac589375df7af07aee45
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-09-24 15:38:12 +0200

    feat: github actions reporter

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
       "!**/__pycache__/**",
       "!**/*.py"
     ],
+    collectCoverage: true,
     coverageDirectory: "<rootDir>/.coverage",
     testPathIgnorePatterns: ["/node_modules/", "/dist/", "/__pycache__/"],
     coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
@@ -16,6 +17,10 @@ module.exports = {
       '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json', diagnostics: false }],
       '^.+\\.js$': 'babel-jest'
     },
-    reporters: ['default', 'summary'],
+    reporters: [
+      'default',
+      'summary',
+      ['github-actions', {silent: false}],
+    ],
     roots: ['<rootDir>/src'],
 };
