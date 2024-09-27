427e51b8602db06514c7b8d2775948f773bac858
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2024-09-27 15:20:58 +0200

    feat: add xray iac

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -42,4 +42,5 @@ resource "aws_lambda_event_source_mapping" "dynamodb_trigger" {
 
 resource "aws_sns_topic" "dynamodb_stream_topic" {
   name = "${local.prefix}url-created"
+  tracing_config = "Active"
 }
---
+++ terraform/xray.tf
@@ -0,0 +1,33 @@
+
+resource "awscc_xray_resource_policy" "xray_policy" {
+  policy_document = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "SNSAccess",
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "sns.amazonaws.com"
+          },
+          "Action": [
+            "xray:PutTraceSegments",
+            "xray:GetSamplingRules",
+            "xray:GetSamplingTargets"
+          ],
+          "Resource": "*",
+          "Condition": {
+            "StringEquals": {
+              "aws:SourceAccount": data.aws_caller_identity.current.account_id
+            },
+            "StringLike": {
+              "aws:SourceArn": "arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${local.prefix}*"
+            }
+          }
+        }
+      ]
+    }
+  )
+  policy_name                 = "${local.prefix}xray-policy"
+  bypass_policy_lockout_check = false
+}
